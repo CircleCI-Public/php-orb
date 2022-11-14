40c2af189976c4e43c16d096a32eecc14f47aa6d
Jaryt <jarytbustard@gmail.com>
2022-11-14 14:40:46 -0500

    fix: use eval

--- src/scripts/install-composer.sh
+++ src/scripts/install-composer.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-ORB_VAL_INSTALL_DIR=$(eval "echo ${ORB_VAL_INSTALL_DIR}")
+INSTALL_DIR=$(eval "echo ${ORB_EVAL_INSTALL_DIR}")
 
 curl -sS https://getcomposer.org/installer -o composer-setup.php
 if [ -n "${ORB_VAL_INSTALL_VERSION}" ]; then
     set -- "$@" --version "${ORB_VAL_INSTALL_VERSION}"
 fi
 
-sudo php composer-setup.php --filename=composer --install-dir="${ORB_VAL_INSTALL_DIR}" "$@"
+sudo php composer-setup.php --filename=composer --install-dir="${INSTALL_DIR}" "$@"
